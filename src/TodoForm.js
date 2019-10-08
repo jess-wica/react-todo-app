8b2dcc8a12669d70fe135b1673925f1faf8ae069
Jessica McKenzie <contact@jessmckenzie.com>
2019-10-08 11:29:23 +0100

    Change Form to controlled component to handle user input

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -2,16 +2,22 @@ import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 class TodoForm extends Component {
-    taskInput = React.createRef();
+    state = { taskInput: '' };
     handleSubmit = (event) => {
         event.preventDefault();
-        console.log(this.taskInput.current.value)
+        console.log(this.state.taskInput);
     }
     render() {
         return (
             <div className="py-3">
                 <form onSubmit={this.handleSubmit} >
-                    <input type="text" className="rounded" placeholder="Enter task..." ref={this.taskInput} required />
+                    <input
+                        type="text"
+                        className="rounded"
+                        value={this.state.taskInput}
+                        onChange={event => this.setState({ taskInput: event.target.value })}
+                        placeholder="Enter task..."
+                        required />
                     <button type="submit" className="rounded">
                         <FontAwesomeIcon icon="plus" />
                     </button>
