200831342f42767ee9c2fa5f9489c7d36689a0ea
Jessica McKenzie <contact@jessmckenzie.com>
2019-10-11 09:26:52 +0100

    remove old commented code from TodoForm component

--- src/TodoForm.js
+++ src/TodoForm.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export class TodoForm extends Component {
     state = {
-        // id: '',
         newItemText: ''
     }
 
@@ -15,7 +14,6 @@ export class TodoForm extends Component {
     createNewToDo = () => {
         this.props.callback(this.state.newItemText);
         this.setState({
-            // id: shortid.generate(),
             newItemText: ''
         });
     }
@@ -34,40 +32,3 @@ export class TodoForm extends Component {
             </button>
         </div>
 }
-
-// class TodoForm extends Component {
-//     state = { taskInput: '' };
-
-//     handleSubmit = (event) => {
-//         event.preventDefault();
-//         this.props.onSubmit({
-//             id: shortid.generate(),
-//             task: this.state.taskInput,
-//             complete: false
-//         });
-//         this.setState({
-//             taskInput: ''
-//         });
-//     }
-//     render() {
-//         return (
-//             <div className="py-3">
-//                 <form onSubmit={this.handleSubmit} >
-//                     <input
-//                         type="text"
-//                         name="taskInput"
-//                         className="rounded"
-//                         value={this.state.taskInput}
-//                         onChange={event => this.setState({ [event.target.name]: event.target.value })}
-//                         placeholder="Enter task..."
-//                         required />
-//                     <button type="submit" className="rounded">
-//                         <FontAwesomeIcon icon="plus" />
-//                     </button>
-//                 </form>
-//             </div>
-//         );
-//     }
-// }
-
-// export default TodoForm;
